20e5315797e44cb80bae73d65ea829b14ca5e72f
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-05-30 20:54:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+const outputDiv = document.querySelector("#output");
+
+function returnPromise(){
+	return new Promise((res, rej) => {
+		setTimeout(()=>{
+			res("Hello world!")
+		},1000)
+	});
+};
+
+returnPromise().then((res) => {
+	outputDiv.innerText = res;
+})
